e8dd46fb61bd7666bee65231072d6a9785ffc011
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-02-01 17:21:22 -0400

    Update image tag extraction in ci_scripts/push_manifest.sh

--- ci_scripts/push_manifests.sh
+++ ci_scripts/push_manifests.sh
@@ -1,5 +1,5 @@
 export ONOS_VERSION=$(grep ONOS_VERSION onos.defs | xargs)
-export IMAGE_TAG=${ONOS_VERSION#*= }
+export IMAGE_TAG="${ONOS_VERSION#*= }"
 
 docker manifest create --amend cachengo/onos:$IMAGE_TAG cachengo/onos-x86_64:$IMAGE_TAG cachengo/onos-aarch64:$IMAGE_TAG
 docker manifest push cachengo/onos:$IMAGE_TAG
